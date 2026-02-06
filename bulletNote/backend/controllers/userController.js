c32c316bb0700da4a4a3e7628f4704bc91d025e4
Chenyang Wang <chenyang.wang@tuni.fi>
2026-02-06 10:09:42 +0200

    try to fix problem

--- bulletNote/backend/controllers/userController.js
+++ bulletNote/backend/controllers/userController.js
@@ -89,7 +89,7 @@ login = async (req, res) => {
             httpOnly: true,
             maxAge: JWT_EXPIRES_IN * 1000,
             secure: isProd,
-            sameSite: 'none',
+            sameSite: isProd ? 'none' : 'lax',
             path: '/'
         }
     );
@@ -117,7 +117,7 @@ getUserInfo = async (req, res) => {
 logout = async (req,res) => {
     res.clearCookie('note_token', {
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: isProd ? 'none' : 'lax',
         secure: isProd,
         path: '/'
     })
